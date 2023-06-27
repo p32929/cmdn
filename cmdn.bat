aca352fd011970bf17359512795cec8c91db95c3
p32929 <p32929@yahoo.com>
2023-06-27 12:08:51 +0600

    no config file

--- cmdn.bat
+++ cmdn.bat
@@ -1,22 +1,11 @@
 @echo off
+setlocal
 
-REM Check if the configuration file exists
-if exist config.txt (
-    REM Read the configuration file to set the SOUND_DIR variable
-    for /f "usebackq tokens=1* delims==" %%G in ("config.txt") do (
-        if /i "%%G"=="SOUND_DIR" set "SOUND_DIR=%%H"
-    )
-)
+REM Get the directory of the batch file
+set "SCRIPT_DIR=%~dp0"
 
-REM Check if the SOUND_DIR variable is set or empty
-if not defined SOUND_DIR (
-    REM Sound file directory not set or empty, prompt for it and save in the configuration file
-    set /p SOUND_DIR=Enter the directory path to the sound file:
-    echo SOUND_DIR=%SOUND_DIR% > config.txt
-) else (
-    REM Sound file directory already set, use the stored value
-    echo Using sound file directory: %SOUND_DIR%
-)
+REM Build the complete path to the sound file
+set "SOUND_FILE=%SCRIPT_DIR%\alarm.mp3"
 
-REM Run the notifier command to play the sound
-start "" /wait /min /b "%SOUND_DIR%"
+REM Play the sound file
+start "" /wait /min /b "%SOUND_FILE%"
